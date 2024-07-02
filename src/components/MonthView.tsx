aadf95b149c42fed2314e4226e98e1d2ee64dbc4
sohyuna0113 <sohyun.ahn@fitnyc.edu>
2024-07-03 00:02:52 +1000

    [ADD] Daily Todo Added

--- src/components/MonthView.tsx
+++ src/components/MonthView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppState } from '../types/types';
+import styles from '../MonthView.module.css';
 
 interface MonthViewProps {
     state: AppState;
@@ -7,14 +8,19 @@ interface MonthViewProps {
 }
 
 const MonthView: React.FC<MonthViewProps> = ({ state, onWeekClick }) => {
+    
+    const weeks = Array(4).fill(null).map((_, index) => `column-${index + 1}`);
+
     return (
         <div>
             <h1> Month View </h1>
-            {state.columnOrder.map(columnId => (
-                <div key={columnId} onClick={() => onWeekClick(columnId)}>
-                    <h2>{state.columns[columnId].title}</h2>
-                </div>
-            ))}
+            <div className={styles.grid}>
+        {weeks.map((weekId, index) => (
+          <div key={index} className={styles.cell} onClick={() => onWeekClick(weekId)}>
+            <h2>{`Week ${index + 1}`}</h2>
+          </div>
+        ))}
+      </div>
         </div>
     )
 };
